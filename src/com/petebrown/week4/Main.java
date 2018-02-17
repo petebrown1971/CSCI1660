d9060f24b2b1da412ad870fd1b7fe148d37eb3d7
petebrown1971 <Thurber2018>
2018-02-16 20:51:32 -0500

    Pete Brown Project 1 (Due 02.12.18)

--- src/com/petebrown/week4/Main.java
+++ src/com/petebrown/week4/Main.java
@@ -8,9 +8,9 @@ import java.util.Hashtable;
 class HashMapPutExample {
 
     public static void main(String[] args) {
-        Hashtable<String, ArrayList<Integer>> names = new Hashtable<>();
+        Hashtable<String, ArrayList<Double>> names = new Hashtable<>();
         String name = "";
-        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Double> temp = new ArrayList<>();
 
         try (Scanner in = new Scanner(System.in)) {
             while (true) {
@@ -18,50 +18,47 @@ class HashMapPutExample {
                 name = in.next();
                 if (name.equals("end")) {
                     break;
+                }
 
+                Scanner input = new Scanner(System.in);
+                double[] avg = new double[5];
 
-                    System.out.println("Enter Monday Average Temp:");
-                    Scanner mon = new Scanner(System.in);
-                    temp.add(mon.nextLine());
-
-                    System.out.println("Enter Tuesday Average Temp:");
-                    Scanner tue = new Scanner(System.in);
-                    temp.add(tue.nextLine());
-
-                    System.out.println("Enter Wednesday Average Temp:");
-                    Scanner wed = new Scanner(System.in);
-                    temp.add(wed.nextLine());
-
-                    System.out.println("Enter Thursday Average Temp:");
-                    Scanner thu = new Scanner(System.in);
-                    temp.add(thu.nextLine());
-
-                    System.out.println("Enter Friday Average Temp:");
-                    Scanner fri = new Scanner(System.in);
-                    temp.add(fri.nextLine());
+                for (int i = 0; i < avg.length; i++) {
+                    System.out.println("Please enter an average daily temperature:");
+                    avg[i] = input.nextDouble();
 
                 }
+
+
             }
         }
     }
-            private static double average(ArrayList <Integer> temp) {
-                // Calculate the summation of the elements in the list
-                long sum = 0;
-                int n = temp.size();
-                for (int i = 0; i < n; i++)
-                    sum += temp.get(i);
-                return ((double) sum) / n;
+ private static double average(ArrayList <Integer> temp) {
+         //Calculate the summation of the elements in the list
+            long sum = 0;
+         double n = temp.size();
+        for (double i = 0; i < n; i++)
+          sum += temp.get(i);
+        return ((double) sum) / n;
 
-
-                // print the hashtable data
-                System.out.println("Hashtable:");
-                {
-
-                    for (String n : names.keySet())
-                        System.out.println("City:" + name + "Average Temp:" + average);
-                }
-            }
+        // print the hashtable data
+        System.out.println("Hashtable:");
+        {
+           for (String name : name.keySet())
+            System.out.println("Key" + name + "value" + n);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
